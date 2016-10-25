2476bf05b21cc8cfa6087614537d63f06d9f7ef7
csiu <celia.sy.siu@gmail.com>
2016-10-25 12:32:45 -0700

    move visualize_state_summary to own file

---
+++ R/visualize_state_summary.R
@@ -0,0 +1,37 @@
+#' Visualize overlaps for all states
+#'
+#' Plot output of \code{tally_state_summary()} into a barchart
+#'
+#' @param input output of \code{tally_state_summary()}
+#'              i.e. dataframe where column 1 is state, and the remaining
+#'              column are the number of bins which overlap 1, 2, ... samples.
+#' @param statepalette character list where each element is a color and
+#'                     the name of each element is a \code{state} level.
+#' @param facet_wrap boolean
+#' @import dplyr
+#' @export
+visualize_state_summary <- function(input, statepalette=NULL, facet_wrap=FALSE){
+  require(ggplot2)
+  dat <-
+    input %>%
+    dplyr::mutate(state = sub("^U", "", state) %>% as.integer()) %>%
+    tidyr::gather(samples, bins, -state) %>%
+    dplyr::mutate(samples = as.integer(samples))
+
+  # Plot
+  plt <-
+    dat %>%
+    ggplot(aes(x = samples, y = bins)) +
+    geom_bar(stat = "identity")
+  if (facet_wrap) {
+    plt <- plt + facet_wrap(~state, scales = "free")
+  } else {
+    plt <- plt + facet_grid(~state)
+  }
+
+  if (!is.null(statepalette)) {
+    CEMTscripts:::plot_colorstrip(dat, plt, statepalette)
+  } else {
+    plt
+  }
+}
