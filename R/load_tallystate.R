ec064b749b91e8d724e49ff71c0b5b15d9eb46e0
csiu <celia.sy.siu@gmail.com>
2016-10-28 10:38:20 -0700

    Generalized function to get and load unionbedg_tally_dir state files

---
+++ R/load_tallystate.R
@@ -0,0 +1,41 @@
+#' Find tally state file in tally dir
+#' @param tally_dir directory containing the state files ending in "bed"
+#' @param state Specify state file to be returned.
+#'              If \code{NULL} (default) is used,
+#'              all files bed files in tally_dir will be returned.
+#' @details
+getfile_tallystate <- function(tally_dir, state=NULL){
+  # List of all files in tally dir
+  # Grep for state of interest
+  input_files <- list.files(path=tally_dir, pattern="*bed")
+
+  if (!is.null(state)){
+    file.path(tally_dir,
+              grep(sprintf("[\\.UE]%s\\.", state), input_files, value=T))
+  } else {
+    input_files
+  }
+}
+
+#  ------------------------------------------------------------------------
+#' Load unionbedg tally state file
+#'
+#' @param input_file TSV file with 6 columns + no header:
+#'                   chrom, start, stop,
+#'                   number of samples, sample representation
+#'                   e.g. 0010 (where 0=absent; 1=present),
+#'                   number of bins in region i.e. (stop-start)/200bp.
+#' @import readr
+load_tallystate <- function(input_file){
+  read_tsv(input_file,
+           col_names = c("chrom", "start", "end",
+                         "samples", "label", "bins"),
+           col_types = cols(
+             chrom = col_character(),
+             start = col_integer(),
+             end = col_integer(),
+             samples = col_integer(),
+             label = col_character(),
+             bins = col_integer()
+           ))
+}
--- R/tally_state_summary.R
+++ R/tally_state_summary.R
@@ -2,26 +2,11 @@
 #'
 #' In a given state, tally the number of bins which overlap
 #'
-#' @import readr
-#' @param input_file TSV file with 6 columns + no header:
-#'                   chrom, start, stop,
-#'                   number of samples, sample representation
-#'                   e.g. 0010 (where 0=absent; 1=present),
-#'                   number of bins in region i.e. (stop-start)/200bp.
+#' @param input_file Format is described in \code{load_tallystate()}
 tally_state <- function(input_file){
   require(dplyr)
   ## The number of bins which agree among n samples
-  dd <- read_tsv(input_file,
-                 col_names = c("chrom", "start", "end",
-                               "samples", "label", "bins"),
-                 col_types = cols(
-                   chrom = col_character(),
-                   start = col_integer(),
-                   end = col_integer(),
-                   samples = col_integer(),
-                   label = col_character(),
-                   bins = col_integer()
-                 )) %>%
+  dd <- CEMTscripts:::load_tallystate(input_file) %>%
     group_by(samples) %>%
     summarize(nbins = sum(bins))
   setNames(dd$nbins, as.character(dd$samples))
@@ -43,16 +28,14 @@ tally_state_summary <- function(tally_dir, out_file=NULL, verbose=FALSE){
   require(dplyr)
 
   # Input files
-  input_files <- list.files(path=tally_dir, pattern="*bed")
+  input_files <- CEMTscripts:::getfile_tallystate(tally_dir, state=NULL)
 
   # Generate summary data
   dat <- NULL
   for (s in 1L:length(input_files)){
     if (verbose) message(paste("Loading state:", s))
     input_file <-
-      file.path(tally_dir,
-                grep(sprintf("[\\.UE]%s\\.", s), input_files, value=T)
-                )
+      CEMTscripts:::getfile_tallystate(tally_dir, state=s)
     dat <-
       rbind(
         dat,
