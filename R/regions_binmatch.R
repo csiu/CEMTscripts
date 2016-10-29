3486e3267d0ac9bd1d11de5de1237dee19a36130
csiu <celia.sy.siu@gmail.com>
2016-10-29 11:23:29 -0700

    Make it so can run bedtools unionbedg

--- R/regions_binmatch.R
+++ R/regions_binmatch.R
@@ -1,3 +1,21 @@
+#' Run Bedtools unionbedg
+#'
+#' @param bgfiles Character list containing the list of BedGraphs to combine.
+#'                The names of the list elements will be used as output
+#'                header names. If no element names are set,
+#'                the file basename will be used instead.
+#' @details Invoke System Command: \code{bedtools unionbedg}
+bedtools_unionbedg <- function(bgfiles, outfile){
+  if (is.null(names(bgfiles))) names(bgfiles)<-basename(bgfiles)
+
+  e <-system2("bedtools",
+              args = c("unionbedg", "-i", bgfiles,
+                       "-header", "-names", names(bgfiles)),
+              stdout = outfile)
+  if (e != 0) stop("Failed to execute 'bedtools unionbedg'")
+}
+
+#  ------------------------------------------------------------------------
 #' State comparison
 #'
 #' Do the states between the segmentation match?
