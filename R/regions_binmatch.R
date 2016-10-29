b0ee38e7fcb4b389059abf814e55ee9decd5498a
csiu <celia.sy.siu@gmail.com>
2016-10-28 17:40:36 -0700

    Init script

---
+++ R/regions_binmatch.R
@@ -0,0 +1,11 @@
+#' State comparison
+#'
+#' Do the states between the segmentation match?
+#'
+#' @details
+#'   Intended to be used to compare the states between the same bins
+#'   of the same sample generated using different models
+regions_binmatch <- function(x.filename, y.filename, x.state, y.state){
+
+}
+#  ------------------------------------------------------------------------
