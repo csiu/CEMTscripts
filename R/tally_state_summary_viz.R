d3b63c3590156fe6c2112ff5074523a0e4d12703
csiu <celia.sy.siu@gmail.com>
2016-11-03 16:09:07 -0700

    Enable ability to log10 transform y axis

--- R/tally_state_summary_viz.R
+++ R/tally_state_summary_viz.R
@@ -8,9 +8,13 @@
 #' @param statepalette character list where each element is a color and
 #'                     the name of each element is a \code{state} level.
 #' @param facet_wrap boolean
+#' @param scale_y_log10
+#'          boolean; when this is TRUE, the bin counts will be
+#'          log10 transformed and the plot will use points instead of bars.
 #' @import dplyr
 #' @export
-tally_state_summary_viz <- function(input, statepalette=NULL, facet_wrap=FALSE){
+tally_state_summary_viz <- function(input, statepalette=NULL,
+                                    facet_wrap=FALSE, scale_y_log10=FALSE){
   require(ggplot2)
   dat <-
     input %>%
@@ -19,10 +23,19 @@ tally_state_summary_viz <- function(input, statepalette=NULL, facet_wrap=FALSE){
     dplyr::mutate(samples = as.integer(samples))
 
   # Plot
-  plt <-
-    dat %>%
-    ggplot(aes(x = samples, y = bins)) +
-    geom_bar(stat = "identity")
+  if (scale_y_log10) {
+    plt <-
+      dat %>%
+      ggplot(aes(x = samples, y = bins, group = state)) +
+      geom_point(size = .75) +
+      geom_line() +
+      scale_y_log10()
+  } else {
+    plt <-
+      dat %>%
+      ggplot(aes(x = samples, y = bins)) +
+      geom_bar(stat = "identity")
+  }
   if (facet_wrap) {
     plt <- plt + facet_wrap(~state, scales = "free")
   } else {
