e89609a2d26cb4166a5b9b77ed86fc5274b00a4c
csiu <celia.sy.siu@gmail.com>
2016-11-04 22:58:11 -0700

    init functions

---
+++ R/transcript_summarize.R
@@ -0,0 +1,17 @@
+#' Create tx2gene for use in tximport
+#' @format a two-column data.frame linking
+#'         transcript id (column 1) to gene id (column 2)
+transcript_create_tx2gene <- function(){
+
+}
+#  ------------------------------------------------------------------------
+#' Map transcripts to genes
+transcript_togene <- function(){
+
+}
+
+#  ------------------------------------------------------------------------
+#' Summarize genes
+transcript_summarize <- function(){
+
+}
