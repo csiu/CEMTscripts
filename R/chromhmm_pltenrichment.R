bd0e9460e333f93fb1f7ed8f5b312d8561b5af3a
csiu <celia.sy.siu@gmail.com>
2016-11-17 14:43:46 -0800

    Remove redundant function

--- R/chromhmm_pltenrichment.R
+++ R/chromhmm_pltenrichment.R
@@ -56,23 +56,3 @@ plot_combinematrix <- function(mat,
       ...)
   }
 }
-
-#  ------------------------------------------------------------------------
-#' Plot overlap enrichment
-#' @param input Output of \code{chromhmm_loadenrichment}
-#' @param ... arguments to pheatmap::pheatmap(...)
-#' @import ggplot2
-#' @export
-chromhmm_pltenrichment <- function(input, ...){
-  input %>%
-    as.data.frame() %>%
-    tibble::column_to_rownames("state") %>%
-    data.matrix() %>%
-    pheatmap::pheatmap(
-      scale = "column",
-      ## colors from: viridis::viridis(5, option="B")
-      # color = colorRampPalette(
-      #   c("#000004FF", "#56106EFF", "#BB3754FF",
-      #     "#F98C0AFF", "#FCFFA4FF"))(100),
-      ...)
-}
