b8b29c817d31e3150e63e7579ea5cbe77d751266
csiu <celia.sy.siu@gmail.com>
2016-12-19 17:04:51 -0800

    init functionality to load broadpeaks

--- R/load_macs2.R
+++ R/load_macs2.R
@@ -17,3 +17,24 @@ load_macs2_narrowpeak <- function(narrowPeak_file) {
     select(dat, -chr, -start, -end, -strand)
   gr
 }
+
+#  ------------------------------------------------------------------------
+#' Load MACS2 NAME_peaks.broadPeak
+#' @source \url{https://github.com/taoliu/MACS}
+#' @export
+load_macs2_broadpeak <- function(broadPeak_file) {
+  dat <-
+    readr::read_tsv(broadPeak_file,
+                    col_names = c(
+                      "chr", "start", "end",
+                      "name", "score", "strand",
+                      "fc", "nlogp", "nlogq"),
+                    col_types = readr::cols(
+                      chr = readr::col_character()),
+                    progress = FALSE)
+  gr <-
+    GenomicRanges::makeGRangesFromDataFrame(dat)
+  GenomicRanges::mcols(gr) <-
+    select(dat, -chr, -start, -end, -strand)
+  gr
+}
