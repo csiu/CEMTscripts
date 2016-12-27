19a27c815a2b40a8d4309658cd48008a05628331
csiu <celia.sy.siu@gmail.com>
2016-12-27 11:38:40 -0800

    update state decisiveness

--- R/state_decisiveness.R
+++ R/state_decisiveness.R
@@ -30,14 +30,28 @@ decisiveness <- function(state_emissions){
 #'          list containing the genomic coverage of the states.
 #'          Used to scale the states.
 #'          By default, there is no scaling
+#' @param returntype specify what value is returned:
+#'                   "d_k" for vector of d_k;
+#'                   "dc_k" for scaled vector of d_k * genomic coverage;
+#'                   anything else (default) for the metric itself
 #' @details Calculated by \code{D = sum[k=1 to K]{d_k * c_k}};
 #'          want to minimize this value.
 #' @examples
 #' emissions_probs <- rbind(c(1, 0.5), c(1,0))
 #' genomic_coverage <- c(.95, .05)
 #' CEMTscripts:::decisiveness_model(emissions_probs, genomic_coverage)
-decisiveness_model <- function(emissions_probs, genomic_coverage=1){
-  sum(apply(emissions_probs, 1, CEMTscripts:::decisiveness) * genomic_coverage)
+decisiveness_model <- function(emissions_probs, genomic_coverage=1,
+                               returntype=""){
+  d_k  <- apply(emissions_probs, 1, CEMTscripts:::decisiveness)
+  dc_k <-  d_k * genomic_coverage
+
+  if (returntype=="d_k") {
+    d_k
+  } else if (returntype=="dc_k") {
+    dc_k
+  } else {
+    sum(dc_k)
+  }
 }
 
 #  ------------------------------------------------------------------------
@@ -48,11 +62,16 @@ decisiveness_model <- function(emissions_probs, genomic_coverage=1){
 #' @param genomic_coverage
 #'          list containing the genomic coverage of the states.
 #'          Used to scale the states.
+#' @param returntype specify what value is returned:
+#'                   "d_k" for vector of d_k;
+#'                   "dc_k" for scaled vector of d_k * genomic coverage;
+#'                   anything else (default) for the metric itself
 #' @details Calculated by \code{D = sum[k=1 to K]{d_k * c_k}} where
 #'          \code{d_k = sum[h=1 to H]{min(1-E_h, E_h)}};
 #'          want to minimize this value.
 #' @export
-state_decisiveness <- function(filename.emissions, genomic_coverage){
+state_decisiveness <- function(filename.emissions, genomic_coverage,
+                               returntype=""){
   ## Load emissions
   CEMTscripts:::load_emissionsdb(filename.emissions) %>%
 
@@ -63,5 +82,5 @@ state_decisiveness <- function(filename.emissions, genomic_coverage){
     tibble::column_to_rownames("state") %>%
 
     ## Calculate the average decisiveness across multiple states
-    CEMTscripts:::decisiveness_model(genomic_coverage)
+    CEMTscripts:::decisiveness_model(genomic_coverage, returntype)
 }
